20c6ccbdcab25a9fe110a5c507f8f6e2026fc762
SangeevanRumex <sangeevan@rumex.lk>
2021-06-16 10:05:29 +0530

    Initialize project

---
+++ src/test/java/com/example/UserManagementSpringWebApp/UserManagementSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserManagementSpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
